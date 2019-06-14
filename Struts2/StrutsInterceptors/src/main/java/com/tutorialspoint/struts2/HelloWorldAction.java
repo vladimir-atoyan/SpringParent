37b2d0f2fbf1cc6a2698ebdd03707eccdedb0e7f
Vladimir Atoyan <vladimir.atoyan@gmail.com>
2019-06-14 12:06:51 +0400

    struts interceptors

---
+++ Struts2/StrutsInterceptors/src/main/java/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -0,0 +1,20 @@
+package com.tutorialspoint.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+    private String name;
+
+    public String execute() throws Exception {
+        System.out.println("Inside action....");
+        return "success";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Struts2/StrutsInterceptors/src/main/webapp/HelloWorld.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head><title>Hello World</title></head>
+
+<body>
+Hello World, <s:property value="name"/>
+</body>
+</html>
